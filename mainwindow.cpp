1e90e9db37bb37742d7298d9e49ba4b6fb73db43
jairLucas <jlucasb@unsa.edu.pe>
2021-12-04 17:53:05 -0500

    Cronometro funcional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,81 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //coneccion del timer para que actualize el cronometro cada segundo
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateTime()));
+    timer->start(1000);
 }
+/*
+ * Metodo para la actualización del cronometro
+*/
+void MainWindow::updateTime(){
+    time=time.addSecs(1);
+    QString time_text = time.toString("m:ss");
+    ui->label_time->setText(time_text);
+}
+
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+/*
+ * Metodos para actualizar el cronometro usando QTimer y QTime
+*/
+/*
+void MainWindow::definirResultadoFinal(){
+
+    //si las piezas tocan el borde superior pierde
+    msgBox.setWindowTitle("Juego Terminado");
+    msgBox.setIcon(QMessageBox::Information);
+    msgBox.setStandardButtons(QMessageBox::Yes);
+    msgBox.addButton(QMessageBox::No);
+    msgBox.setDefaultButton(QMessageBox::Yes);
+    msgBox.setEscapeButton(QMessageBox::No);
+}
+*/
+
+/*
+ * #############################################################
+*/
+
+void MainWindow::on_pushButton_quit_clicked()
+{
+    QCoreApplication::quit();
+}
+
+
+void MainWindow::on_pushButton_pause_clicked()
+{
+    //detener el cronometro
+    timer->stop();
+
+    //mostrar mensaje de pausa del cronometro
+    msgBox.setWindowTitle("PAUSA");
+    msgBox.setIcon(QMessageBox::Information);
+    msgBox.setStandardButtons(QMessageBox::Ok);
+    msgBox.setDefaultButton(QMessageBox::Ok);
+    msgBox.setText("Juego Pausado\n¿Continuar?");
+    if (QMessageBox::Ok == msgBox.exec()){
+        timer->start();
+    }
+}
+
+
+void MainWindow::on_pushButton_start_clicked()
+{
+    //inicializando el cronometro
+    time.setHMS(0,0,0);
+    QString time_text = time.toString("m:ss");
+    ui->label_time->setText(time_text);
+
+}
+
+void MainWindow::on_pushButton_restart_clicked()
+{
+    time.setHMS(0,0,0);
+    QString time_text = time.toString("m:ss");
+    ui->label_time->setText(time_text);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QTime>
+#include <QDateTime>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +18,20 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QTimer *timer{new QTimer(this)}; //Repetir una accion cierta cantidad de tiempo
+    QTime time; //Objeto que representa el reloj
+    unsigned lines{0};  //contador del numero de lineas
+    QMessageBox msgBox;
+
+private slots:
+    //slots propios
+    void updateTime();
+    //slots automaticos
+    void on_pushButton_quit_clicked();
+    void on_pushButton_pause_clicked();
+    void on_pushButton_start_clicked();
+
+    void on_pushButton_restart_clicked();
 
 private:
     Ui::MainWindow *ui;
